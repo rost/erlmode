c3c5c5233aa1298e83a1d2a0f6ee51abd4ee79e1
Robin Osterberg <roesterberg@gmail.com>
2012-11-17 02:11:02 +0100

    Add erlmode.el main module

---
+++ elisp/erlmode.el
@@ -0,0 +1,20 @@
+;;; erlmode.el --- Major mode for editing and running Erlang
+
+;; -*- coding: utf-8; lexical-binding: t -*-
+
+;; Commentary:
+;; A new and in time hopefully better major-mode for editing Erlang.
+
+;;; Code:
+
+(eval-when-compile (require 'cl))
+
+(require 'erl-helpers)
+
+;;;_* erlmode ==================================================================
+;; setup of erlang-mode
+(require 'erl-legacy)
+
+(provide 'erlmode)
+
+;;; erlmode.el ends here
