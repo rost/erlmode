206d6dd43e6a112aa3acc47e92539680fad90b54
Robin Osterberg <roesterberg@gmail.com>
2012-11-17 02:05:13 +0100

    Add erl-helpers.el
    Add functions for converting mfa strings to cells and back.
    Add functions for grabbing module name of the current buffer and module
    filename of the current or other buffer.
    Add function to get directory path of an  elisp module on disk

---
+++ elisp/erl-helpers.el
@@ -0,0 +1,46 @@
+;;; erl-helpers.el --- Helper functions
+
+;;;_+ mfa cell handling --------------------------------------------------------
+(defun erl--mfa-cell-to (type cell)
+  "Convert mfa cell or string to format specified by `type'."
+  (destructuring-bind (m f a)
+      (erl--mfa-split (if (listp cell) (erl--mfa-cell-to-string cell) cell))
+    (case type
+      ('m   (format "%s" m))
+      ('mf  (format "%s:%s" m f))
+      ('mfa (format "%s:%s/%s" m f a))
+      ('f   (format "%s" f))
+      ('fa  (format "%s/%s" f a)))))
+
+(defun erl--mfa-string-to-cell (mfa)
+  "Convert string `mod:func/1' to (`mod' `func' `1')."
+  (destructuring-bind (m f a) (erl--mfa-split mfa)
+    (list m f a)))
+
+(defun erl--mfa-cell-to-string (cell)
+  "Convert cell (`mod' `func' `1') to `mod:func/1'."
+  (destructuring-bind (m f a) cell
+    (format "%s:%s/%s" m f a)))
+
+(defun erl--mfa-split (mfa)
+  "Split string `mod:func/1' to (`mod' `func' `1')"
+  (destructuring-bind (m f a) (split-string mfa ":\\|/")
+    (list m f (string-to-int a))))
+
+;;;_+ module handling ----------------------------------------------------------
+(defvar erl--file-name-extension-re "\\.[eh]rl$"
+  "Regex matching Erlang source file extensions.")
+
+(defun erl--module-name ()
+  (file-name-nondirectory (substring-no-properties (buffer-name) 0 -4)))
+
+(defun erl--module-file-name (&optional module)
+  (concat (or module (erl--module-name)) ".erl"))
+
+(defun erl--file-name ()
+  (file-name-nondirectory (buffer-name)))
+
+
+(provide 'erl-helpers)
+
+;;; erl-helpers.el ends here
