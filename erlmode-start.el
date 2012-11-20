64663483cace494c650cce2cdab5ece023b135ec
Robin Osterberg <roesterberg@gmail.com>
2012-11-20 07:51:32 +0100

    Add erlmode-start.el

---
+++ erlmode-start.el
@@ -0,0 +1,28 @@
+;;; erlmode-start.el --- Major mode for editing and running Erlang
+
+;; -*- coding: utf-8; lexical-binding: t -*-
+
+;; Commentary:
+;; A new and in time hopefully better major-mode for editing Erlang.
+
+;; Installation:
+;; (add-to-list 'load-path "~/path/to/erlmode-root")
+;; (require 'erlmode-start)
+
+;;; Code:
+
+;;;_+ utils --------------------------------------------------------------------
+
+(defun erl--path-of-library (lib)
+  (file-truename (file-name-directory (locate-library lib))))
+
+;;;_+ initial setup ------------------------------------------------------------
+
+(add-to-list 'load-path (concat (erl--path-of-library "erlmode-start")
+                                "elisp"))
+
+(require 'erlmode)
+
+(provide 'erlmode-start)
+
+;;; erlmode-start.el ends here
