7c511c1876f2dd6e04d9158b2e3e4653ab6f0dbd
Robin Osterberg <roesterberg@gmail.com>
2012-12-09 19:14:07 +0100

    Update hippie expand for external calls
    Make the try function only work when standing on `mod:fun' symbol.

--- elisp/contrib/he-erlmode.el
+++ elisp/contrib/he-erlmode.el
@@ -12,25 +12,28 @@
 ;;;_+ try functions ------------------------------------------------------------
 (defun try-expand-erlmode-complete-external-call (old)
   "Complete Erlang module and function names using erlmode as backend."
-  (unless old
-    (let* ((end (point))
-           (beg (save-excursion (while (not (looking-at ":"))
-                                  (backward-char)) (point))))
-      (he-init-string beg end))
-    (setq he-expand-list (erl-candidates)))
-  (while (and he-expand-list
-              (he-string-member (car he-expand-list) he-tried-table))
-    (setq he-expand-list (cdr he-expand-list)))
-  (if (null he-expand-list)
-      (progn
-        (when old (he-reset-string))
-        nil)
-    (he-substitute-string (concat ":" (car he-expand-list) "()"))
-    (backward-char)
-    (setq he-tried-table  (cons (car he-expand-list) (cdr he-tried-table)))
-    (setq he-expand-list  (cdr he-expand-list))
-    (setq he-num 0)
-    t))
+  (when (and (looking-at ")") (looking-back "("))
+    (backward-char))
+  (when (erl--complete-type-mf-p)
+    (unless old
+      (let* ((end (point))
+             (beg (save-excursion (while (not (looking-at ":"))
+                                    (backward-char)) (point))))
+        (he-init-string beg end))
+      (setq he-expand-list (erl-candidates)))
+    (while (and he-expand-list
+                (he-string-member (car he-expand-list) he-tried-table))
+      (setq he-expand-list (cdr he-expand-list)))
+    (if (null he-expand-list)
+        (progn
+          (when old (he-reset-string))
+          nil)
+      (he-substitute-string (concat ":" (car he-expand-list) "()"))
+      (backward-char)
+      (setq he-tried-table  (cons (car he-expand-list) (cdr he-tried-table)))
+      (setq he-expand-list  (cdr he-expand-list))
+      (setq he-num 0)
+      t)))
 
 ;;;_+ setup --------------------------------------------------------------------
 (defun turn-on-hippie-expand-erlmode ()
